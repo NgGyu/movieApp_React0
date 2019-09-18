f819e818760d67ccae015d2d4d2e3ab4dc4b4494
You Eung Gyu <aba1212@yonsei.ac.kr>
2019-09-18 09:34:20 +0900

    Updates

---
+++ precache-manifest.2be5342e61d7ac81a095bf615e344ba8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd612281f79a77d22547196b1d944e5",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "4c5dab28f95cb1077c05",
+    "url": "/movie_app_2019/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "bdd8a808aa278518fafc",
+    "url": "/movie_app_2019/static/js/2.86a49d24.chunk.js"
+  },
+  {
+    "revision": "4c5dab28f95cb1077c05",
+    "url": "/movie_app_2019/static/js/main.c940d5ef.chunk.js"
+  },
+  {
+    "revision": "29aaad419714eb018816",
+    "url": "/movie_app_2019/static/js/runtime~main.f4904fd8.js"
+  }
+]);
